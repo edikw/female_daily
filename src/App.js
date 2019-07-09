cce332e610807fb0f3c04e15685bc641ee8373cd
edikw <edikurniawanwibowo@gmail.com>
2019-07-09 15:21:32 +0700

    change title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { HashRouter as Router, Route} from "react-router-dom";
 import landingPage from './pages/landingPage';
